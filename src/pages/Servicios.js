04ad9e008b2294add606cc0b683457c7b603c198
David Flores <davidafloresvel@gmail.com>
2022-03-01 17:21:30 -0700

    servicios

--- src/pages/Servicios.js
+++ src/pages/Servicios.js
@@ -3,7 +3,18 @@ import React from 'react'
 export const Servicios = () => {
     return (
         <div>
-            <h1>Servicios</h1>
+            <ul>
+                <li>Detallado Estético exterior </li>
+                <li>Derallado Estético Interior</li>
+                <li>Encerado</li>
+                <li>Descontaminación</li>
+                <li>Protección cerámica basé silicio</li>
+                <li>Protección cerámica base grafeno</li>
+                <li>Audio</li>
+                <li>Accesorios</li>
+                <li>Alarmas</li>
+                <li>Polarizado</li>
+            </ul>
         </div>
     )
 }
