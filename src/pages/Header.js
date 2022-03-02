964d424889f2ed1d43226a471dcebbc16a9198f3
David Flores <davidafloresvel@gmail.com>
2022-03-02 12:39:34 -0700

    servicios

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t1.6435-9/170693964_886516878571504_3767216802158685553_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_ohc=DTzVJa5ujuwAX9_ah6H&_nc_ht=scontent-den4-1.xx&oh=00_AT9EFjoxbNXOIWPE843cwm-yTspFU5xCE6ZUGEkvhR9ucg&oe=6243AE1D" alt="" class="rounded mx-auto d-block" />
+            <img className="img1" src="https://scontent-den4-1.xx.fbcdn.net/v/t1.6435-9/170693964_886516878571504_3767216802158685553_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_ohc=DTzVJa5ujuwAX9_ah6H&_nc_ht=scontent-den4-1.xx&oh=00_AT9EFjoxbNXOIWPE843cwm-yTspFU5xCE6ZUGEkvhR9ucg&oe=6243AE1D" alt="" class="rounded mx-auto d-block" />
         </div>
     )
 }
