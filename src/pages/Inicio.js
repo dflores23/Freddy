c80c1e0df8da9dcc3b2e6aa6f8d4ea16d9aab506
David Flores <davidafloresvel@gmail.com>
2022-03-16 17:10:43 -0600

    new video

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -6,7 +6,7 @@ export default function Inicio() {
     return (
         <div class="text-center">
             <p class="one">Somos una empresa de estética automotriz, donde lo más importante es satisfacer a los clientes.</p>
-            <Iframe url="https://www.facebook.com/plugins/video.php?height=317&href=https%3A%2F%2Fwww.facebook.com%2Ffreddysautodetail%2Fvideos%2F697297488106616%2F&show_text=false&width=560&t=0" width="450px" height="450px" id="myId" className="video" display="initial" ></Iframe>
+            <Iframe url="https://www.facebook.com/plugins/video.php?height=317&href=https%3A%2F%2Fwww.facebook.com%2Ffreddysautodetail%2Fvideos%2F697297488106616%2F&show_text=false&width=560&t=0" width="90%" height="410px" id="myId" className="video" display="initial" position="relative"></Iframe>
         </div>
     )
 }
