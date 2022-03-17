aa238decd19cc5e3fe9f7df2a92ede4ffba8c195
David Flores <davidafloresvel@gmail.com>
2022-03-16 18:14:08 -0600

    new video

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Header() {
     return (
         <header>
-           <img  className="two" src="https://scontent.xx.fbcdn.net/v/t1.15752-9/275450742_4992788550805336_6798419080914109545_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=aee45a&_nc_ohc=VNEPaGzLsWIAX_qM8sv&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVLm84AYYf4RYaoPN69LGcLgkTFtnxYKmix3TpYCr6S_8g&oe=6250AE79" alt=""/>
+           <img  className="header" src="https://scontent.xx.fbcdn.net/v/t1.15752-9/275450742_4992788550805336_6798419080914109545_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=aee45a&_nc_ohc=VNEPaGzLsWIAX_qM8sv&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVLm84AYYf4RYaoPN69LGcLgkTFtnxYKmix3TpYCr6S_8g&oe=6250AE79" alt=""/>
         </header>
     );
 }
