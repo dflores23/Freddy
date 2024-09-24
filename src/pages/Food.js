1bf3463b26a3987595c814fc17dd66985cd2e63f
David Flores <davidafloresvel@gmail.com>
2024-09-23 18:31:03 -0600

    food updated

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -5,9 +5,19 @@ import React from 'react'
 export default function Food() {
     return (
        <>
+       <div>
        <h1>
-        Food will go here
+        Food Menu
        </h1>
+       <p>
+        Chilaquiles verdes $15.00
+       </p>
+       <p>
+        Chilaquiles de Mole $15.00
+       </p>
+
+       </div>
+
        </>
         
     )
